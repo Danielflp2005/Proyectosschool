8ad0be5f49ad418446d5540ec235142d75032c84
«Daniel <davi308@vidalibarraquer.net>
2025-09-29 13:50:18 +0200

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/ProjectJava1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+import static java.lang.System.in;
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumne
+ */
+public class ProjectJava1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");   
+        Scanner teclado = new Scanner(in);
+        String casa = teclado.next();
+        System.out.println(casa);
+
+    }
+}
